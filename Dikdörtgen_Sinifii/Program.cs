dc22583edd005fbcbd17319cdcf2632037c7bd46
Ensar Ünal <Ensar nal@DESKTOP-9H5GS5R>
2022-12-17 17:44:36 +0300

    Proje dosyası ekle.

---
+++ Dikdörtgen_Sinifii/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dikdörtgen_Sinifii
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen(3, 4);
+            Console.WriteLine("Dikdörtgenin alanı: {0}", dikdortgen.AlanHesapla());
+            Console.WriteLine("Dikdörtgenin çevresi: {0}", dikdortgen.CevreHesapla());
+            Console.ReadLine();
+
+        }
+    }
+    class Dikdortgen
+    {
+        private int a, b;
+        public Dikdortgen(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public int AlanHesapla()
+        {
+            return a * b;
+        }
+        public int CevreHesapla()
+        {
+            return 2 * (a + b);
+        }
+    }
+}
